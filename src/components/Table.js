241fbd6b2e972467c72358cbca5dfb50a6591675
jglynn1001 <joannaglynn@yahoo.com>
2023-07-23 19:49:51 -0500

    fixing table

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,28 +10,28 @@ function statusChange(data) {
 const Table = ({ sat }) => {
   return (
     <div>
-      {sat.map((data, id) => {
-        return (
-          <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type of Satellite </th>
-                <th>Launch Date</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type of Satellite </th>
+            <th>Launch Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sat.map((data, id) => {
+            return (
               <tr key={id}>
                 <td>{data.name}</td>
                 <td>{data.type}</td>
                 <td>{data.launchDate}</td>
                 <td>{statusChange(data)}</td>
               </tr>
-            </tbody>
-          </table>
-        );
-      })}
+            );
+          })}
+        </tbody>
+      </table>
       ;
     </div>
   );
